8c399c31466b893d497daf4db086970b937301a4
Mattias Jansson <matja307@student.liu.se>
2013-10-03 12:16:30 +0200

    Added counting loop

--- mainProject/mainProject/mainProject.cpp
+++ mainProject/mainProject/mainProject.cpp
@@ -6,6 +6,11 @@ int main(){
 
 	cout << "Apple pie is Awesome" << endl;
 	cout << "Hello, apple pie!" << endl;
+    
+    for (int i = 0; i<4; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
 
 	system("pause");
 	return 0;
