f9db309cd792bae88f6ca46a5a973008d44a8d60
Mattias Jansson <matja307@student.liu.se>
2013-10-09 13:44:52 +0200

    Minor changes

--- mainProject/mainProject/mainProject.cpp
+++ mainProject/mainProject/mainProject.cpp
@@ -4,14 +4,8 @@ using namespace std;
 
 int main(){
 
-	cout << "Apple pie is Awesome" << endl;
-	cout << "Hello, apple pie!" << endl;
+	cout << "Hello, World!" << endl;
     
-    for (int i = 0; i<4; i++) {
-        cout << i << " ";
-    }
-    cout << endl;
-
 	system("pause");
 	return 0;
 }
