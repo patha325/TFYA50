11b7940b99a63cb50c9affba1a76438427aa6a7d
Mattias Jansson <matja307@student.liu.se>
2013-10-09 13:37:22 +0200

    Some code

--- startingProject.cpp
+++ startingProject.cpp
@@ -4,9 +4,21 @@
 
 using namespace std;
 
+void hideNSeek();
+
 int main(){
 
 	cout << "Hello, World!" << endl;
+    
+    
 	system("pause");
 	return 0;
+}
+
+void hideNSeek(){
+
+    for (int i = 0; i<10; i++) {
+        cout << i << endl;
+    }
+    cout << "Ready or not, here I come!" << endl;
 }
