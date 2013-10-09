c286b5c44be10cd3dd31d7520c927f2bd4719580
unknown <maror634@IFMVAK103.ad.liu.se>
2013-10-09 14:50:43 +0200

    Added Hide and seek fcn

--- mainProject/mainProject/mainProject.cpp
+++ mainProject/mainProject/mainProject.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+void hideNSeek();
+
 int main(){
 
 	cout << "Hello, World!" << endl;
-    
+
+
+    hideNSeek();
 	system("pause");
 	return 0;
+}
+
+void hideNSeek(){
+
+	for (int i = 0; i<10; i++){
+	
+		cout << i << endl;
+	}
+	cout << "Ready or not, here I come!" << endl;
 }
