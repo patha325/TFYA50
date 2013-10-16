d25e01c8bd0ec5a9a112e677c03ac08cbff8a041
Patrik <patha325@IFMVAK102.ad.liu.se>
2013-10-16 12:23:34 +0200

    Testar conflict

--- Proj0.001/Proj0.001/main.cpp
+++ Proj0.001/Proj0.001/main.cpp
@@ -6,13 +6,13 @@ int main(){
 	//initializegraphics
 
 //	Simulation simulation1(0,0,0,0.1,0.1,0.1,0.1,0.1,0.1,"Hej!",true);
-	float a = 0.5; // Watch out for double warning!
+	float a = 0.1; // Watch out for double warning!
 	Simulation* simulation2 = new Simulation(0,0,0,a,a,a,a,a,a,"Hej!",true);
 	simulation2->run_simulation();
 	
 //	Simulation simulation2(resume.simulation1);
 
-
+	
 	
 	return 0;
 }
