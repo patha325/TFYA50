0e6fedc71b0e9b1c2b4687f0fd1cfd5c5e6e5e9e
Mattias Jansson <matja307@student.liu.se>
2013-10-16 13:07:33 +0200

    More comments, and changes in cell_list.h

--- Proj0.001/Proj0.001/cell_list.h
+++ Proj0.001/Proj0.001/cell_list.h
@@ -1,26 +1,26 @@
 #ifndef CELL_LIST_H
 #define CELL_LIST_H
 
-
 #include <vector>
-
 #include "cell.h"
 
 class Cell_list{
 private:
-	float cutoff;
-	float cell_length;
+	//Variables
+	float cutoff; //Potential cutoff
+	float cell_length; //Length of side of cell
+	std::vector<Cell*> list_of_cells; //Vector containing all cells
 
-	std::vector<Cell*> list_of_cells;
-	void clear_cells();
+	//Methods
+	void clear_cells(); 
 	void create_cell();
 
 public:
-
-	Cell_list(float cutoff); //Creates all cells 
+	//Methods
+	Cell_list(float cutoff); 
 	~Cell_list();
 	void add_atoms_to_cells(std::vector<Atom*>);
-	std::vector<Atom*> get_neighbours(Atom* atom); //Returns vector of atoms in nearest cells, with respect to paramteter atom.
+	std::vector<Atom*> get_neighbours(Atom* atom); 
 };
 
 #endif
