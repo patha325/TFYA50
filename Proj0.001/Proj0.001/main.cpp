0b0fd156056ec1977d842e29c996f94e821a1848
Mattias Jansson <matja307@student.liu.se>
2013-10-16 12:23:42 +0200

    Conflict?

--- Proj0.001/Proj0.001/main.cpp
+++ Proj0.001/Proj0.001/main.cpp
@@ -6,7 +6,7 @@ int main(){
 	//initializegraphics
 
 //	Simulation simulation1(0,0,0,0.1,0.1,0.1,0.1,0.1,0.1,"Hej!",true);
-	float a = 0.5; // Watch out for double warning!
+	float a = 0.2; // Watch out for double warning!
 	Simulation* simulation2 = new Simulation(0,0,0,a,a,a,a,a,a,"Hej!",true);
 	simulation2->run_simulation();
 	
