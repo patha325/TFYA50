7fe079076d648e304fd40786ad82f562346e7b0f
Mattias Jansson <matja307@student.liu.se>
2013-11-05 10:59:03 +0100

    Add atoms to the cell list

--- Proj0.001/Proj0.001/cell_list.h
+++ Proj0.001/Proj0.001/cell_list.h
@@ -17,11 +17,10 @@ private:
     float bulk_length_y;
     float bulk_length_z;
 	std::vector<Cell*> list_of_cells; //Vector containing all cells
-    std::map<int,std::vector<Cell*>> number_to_cell_vector_map;
+    std::map<int,std::vector<Cell*>> number_to_cell_vector_map; //Vector with neighbouring cells
     
 	//Methods
-	void clear_cells(); 
-	void create_cells();
+	void clear_cells();
     void add_to_map(int,std::vector<Cell*>);
 
 public:
@@ -29,6 +28,7 @@ public:
 	Cell_list(float cutoff, int unit_cells_x, int unit_cells_y, int unit_cells_z, float lattice_constant); // unit_cells is a material parameter.
 	~Cell_list();
 	void add_atoms_to_cells(std::vector<Atom*>);
+    void create_cells();
 	std::vector<Atom*> get_neighbours(Atom* atom); 
 };
 
